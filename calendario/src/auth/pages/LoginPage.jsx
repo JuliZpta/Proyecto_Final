e19838972a9a268c13da442a0a736d198faa96ec
JuliZpta <zapatajulian42@gmail.com>
2023-11-18 13:00:28 -0500

    Login

--- calendario/src/auth/pages/LoginPage.jsx
+++ calendario/src/auth/pages/LoginPage.jsx
@@ -1,158 +1,38 @@
 import React from 'react';
 import './LoginPage.css';
-import { useAuthStore, useForm } from '../../hooks';
-import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import Swal from 'sweetalert2';
-import 'sweetalert2/dist/sweetalert2.min.css'
+import { useNavigate } from 'react-router-dom'; // Cambia a useNavigate
 
 
-const loginFormFields = {
-    loginEmail:     '',
-    loginPassword:  '',
-}
-
-const registerFormFields = {
-    registerName:       '',
-    registerEmail:      '',
-    registerPassword:   '',
-    registerPassword2:  '',
-}
+// Resto de tu código
 
 export const LoginPage = () => {
+    const navigate = useNavigate(); // Reemplaza useHistory con useNavigate
 
-    const { startLogin , startRegister ,  errorMessage} = useAuthStore()
+    const handleDirectAccess = () => {
+        // Puedes ejecutar cualquier lógica adicional aquí si es necesario
+        // ...
 
-    const {
-        loginEmail,
-        loginPassword,
-        formState: loginFormState,
-        onInputChange : onLoginInputChange,
-        onResetForm: onLoginResetForm,
-        isFormValid : isLoginFormValid
-    } = useForm(loginFormFields)
-    
-    const handleLoginSubmit = (e) => {
-        e.preventDefault()
-        startLogin({email: loginEmail, password: loginPassword})
-    }
-    
-    const {
-        registerName,
-        registerEmail,
-        registerPassword,
-        registerPassword2,
-        formState: registerFormState,
-        onInputChange : onRegisterInputChange,
-        onResetForm: onRegisterResetForm,
-        isFormValid : isregisterFormValid
-    } = useForm(registerFormFields)
-    
-    const handleRegisterSubmit = (e) => {
-        // TODO: Handle form validations
-        e.preventDefault()
-
-        if (registerPassword !== registerPassword2) return Swal.fire('Registration Error', 'Passwords not match', 'error')
-
-        startRegister({name: registerName, email: registerEmail, password: registerPassword})
-    }
-
-    useEffect(() => {
-        if (errorMessage !== undefined ) Swal.fire('Authentication Error', errorMessage, 'error')
-    }, [errorMessage])
-    
+        // Redirige al usuario al calendario
+        navigate('../calendar'); // Reemplaza '/calendario' con la ruta real de tu calendario
+    };
 
     return (
         <div className="container login-container">
             <div className="row">
-                <div className="col-md-6 login-form-1">
-                    <h3>Log In</h3>
-                    <form onSubmit={handleLoginSubmit}>
+                <div className="col-md-12 login-form">
+                    <h3>Acceder al Calendario</h3>
+                    <form>
                         <div className="form-group mb-2">
                             <input 
-                                type="text"
-                                value={loginEmail}
-                                className="form-control"
-                                placeholder="Email"
-                                onChange={onLoginInputChange}
-                                name="loginEmail"
-                            />
-                        </div>
-                        <div className="form-group mb-2">
-                            <input
-                                type="password"
-                                value={loginPassword}
-                                className="form-control"
-                                placeholder="Password"
-                                onChange={onLoginInputChange}
-                                name="loginPassword"
-                            />
-                        </div>
-
-                        <div className="form-group mb-2">
-                            <input 
-                                type="submit"
+                                type="button"
                                 className="btnSubmit"
-                                value="Login" 
+                                value="Acceder al Calendario"
+                                onClick={handleDirectAccess}
                             />
-                        </div>
-                    </form>
-                </div>
-
-                <div className="col-md-6 login-form-2">
-                    <h3>Sign Up</h3>
-                    <form onSubmit={handleRegisterSubmit}>
-                        <div className="form-group mb-2">
-                            <input
-                                type="text"
-                                value={registerName}
-                                className="form-control"
-                                placeholder="Name"
-                                name="registerName"
-                                onChange={onRegisterInputChange}
-                            />
-                        </div>
-                        <div className="form-group mb-2">
-                            <input
-                                type="email"
-                                value={registerEmail}
-                                className="form-control"
-                                placeholder="Email"
-                                name="registerEmail"
-                                onChange={onRegisterInputChange}
-                            />
-                        </div>
-                        <div className="form-group mb-2">
-                            <input
-                                type="password"
-                                value={registerPassword}
-                                className="form-control"
-                                placeholder="Contraseña" 
-                                name="registerPassword"
-                                onChange={onRegisterInputChange}
-                            />
-                        </div>
-
-                        <div className="form-group mb-2">
-                            <input
-                                type="password"
-                                value={registerPassword2}
-                                className="form-control"
-                                placeholder="Repeat Password" 
-                                name="registerPassword2"
-                                onChange={onRegisterInputChange}
-                            />
-                        </div>
-
-                        <div className="form-group mb-2">
-                            <input 
-                                type="submit" 
-                                className="btnSubmit" 
-                                value="Crear cuenta" />
                         </div>
                     </form>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
